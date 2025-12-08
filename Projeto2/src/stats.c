1805bfd59b1a5ab04c17e50ccc9bc297d783f72a
MarioSantoscv <mariosantos1@ua.pt>
2025-12-08 12:48:16 +0000

    starting feature3 fixxx

---
+++ Projeto2/src/stats.c
@@ -0,0 +1,31 @@
+#include "stats.h"
+#include <semaphore.h>
+
+
+void stats_increment_active(shared_data_t* shared, semaphores_t* sems) {
+    sem_wait(sems->stats_mutex);
+    shared->stats.active_connections++;
+    sem_post(sems->stats_mutex);
+}
+
+
+void stats_decrement_active(shared_data_t* shared, semaphores_t* sems) {
+    sem_wait(sems->stats_mutex);
+    shared->stats.active_connections--;
+    sem_post(sems->stats_mutex);
+}
+
+
+void stats_record_response(shared_data_t* shared, semaphores_t* sems, int status, long bytes) {
+    sem_wait(sems->stats_mutex);
+    shared->stats.total_requests++;
+    shared->stats.bytes_transferred += bytes;
+    if (status == 200)
+        shared->stats.status_200++;
+    else if (status == 404)
+        shared->stats.status_404++;
+    else if (status == 500)
+        shared->stats.status_500++;
+    //if needed to add in the future other codes just add here(ask teacher about this)
+    sem_post(sems->stats_mutex);
+}
