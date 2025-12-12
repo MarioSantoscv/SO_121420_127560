8885cdc1613e898cb16864ae7be046f928cebdb4
MarioSantoscv <mariosantos1@ua.pt>
2025-12-12 20:07:02 +0000

    final version

--- Projeto2/www/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector("button");
-  if (!btn) return;
-
-  btn.addEventListener("click", () => {
-    alert("Button clicked!");
-  });
-});
